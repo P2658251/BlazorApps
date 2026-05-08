3af13a509739b713f614aaf120551cd0f8b65d46
edwardcooper <edward.cooper2002@gmail.com>
2026-05-08 01:19:43 +0100

    Learning how to have an optional parameter in a webpage route

--- BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6d28a1c8c87a7832b7c332b14b07aa7d92a5d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20de8306e4e4192054267412edfccca023b77e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
