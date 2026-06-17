aa59a30f139e930b788274215381bff246dc383c
edwardcooper <edward.cooper2002@gmail.com>
2026-06-17 03:51:51 +0100

    Adding a todo page that will allow userss to add and check tasks that need doing

--- BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c621255254ecd0edd77787a111ab2102bb68c658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87660c6fa5ca8910b5cb6e8532826a4f42d7bb0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
