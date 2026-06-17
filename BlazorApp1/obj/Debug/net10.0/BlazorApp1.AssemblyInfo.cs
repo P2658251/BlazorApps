ab7117a7eac1a5bc58ebaec943eff0530edcf7dd
edwardcooper <edward.cooper2002@gmail.com>
2026-06-17 03:30:38 +0100

    Looking into binding variables to components

--- BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605ba8a31cb574760e3174730d062e0954bab586")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c621255254ecd0edd77787a111ab2102bb68c658")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
